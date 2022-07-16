76e897d2473d63cf18fd05b93eb834749a2a38a4
Pranavvks <pranav.vks@gmail.com>
2022-07-17 00:32:25 +0530

    Smart Contract : I

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,44 +5,132 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol"; 
+
+
+/**
+@dev For the royalty implementation we are using the ERC2981 standard
+
+ */
+
+
 
 // We are inheriting ERC721URIStorage instead of ERC721
 // This will allow us to use the _setTokenURI() function
-contract NFT is ERC721URIStorage {
+
+/**
+
+TO DO :
+
+NFT Marketplace contract would deal with the transfer of funds.
+1. The NFT mint function can only be called by the owner of the 
+   collection.
+2. Read about the ERC 2981 standard of royalty implementation. 
+   Setup a standard for the royalty function.
+3. Implement events , modifiers and errors 
+4. 
+
+ */
+
+contract NFT is ERC721URIStorage, ERC2981 , Ownable {
    using Counters for Counters.Counter;
    Counters.Counter private _tokenIds;
 
-   address public artist;
-   address public txFeeToken;
+   address payable public  royaltyReceiver; // changed the royaltyReceiver address to payable  
+   address public txFeeToken; // ??
    uint256 public txFeeAmount;
+   
    mapping(address => bool) public excludedList;
+   event NFT_Minted(uint256 indexed tokenId);
+  
+   
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    } // Need to check what this is :)
 
-   constructor(
-    address _artist, 
+
+  //  struct NFTAttributes
+  //    {
+  //       uint256 tokenId ;
+  //       uint256 tokenName ;
+  //       string ImageURI ;
+  //       string tokenURI  ;
+  //       uint256 price ;
+  //    }
+
+
+    constructor(
+    address payable _royaltyReceiver, 
     address _txFeeToken,
     uint _txFeeAmount
   ) ERC721('RandomAritst', 'ABC') {
-    artist = _artist;
+    royaltyReceiver = _royaltyReceiver;
     txFeeToken = _txFeeToken;
     txFeeAmount = _txFeeAmount;
-    excludedList[_artist] = true;
+    excludedList[_royaltyReceiver] = true;
   }
 
    function setExcluded(address excluded, bool status) external {
-    require(msg.sender == artist, 'artist only');
+    require(msg.sender == royaltyReceiver, 'royaltyReceiver only');
     excludedList[excluded] = status;
   }
 
-   function mintNFT(address recipient, string memory tokenURI)
+   function mintNFT(string memory tokenURI)
        public
        returns (uint256)
    {
        _tokenIds.increment();
        uint256 newItemId = _tokenIds.current();
-       _safeMint(recipient, newItemId);
+       _safeMint(royaltyReceiver, newItemId);  
+       /**
+       @dev
+       / All tokens will be minted to the owner / royaltyReceiver address.
+      // A user is not going to interact with the NFT contract. 
+      // Interactions will be made using the NFTmarketplace contract. 
+        */
        _setTokenURI(newItemId, tokenURI);
-       return newItemId;
+       emit NFT_Minted(newItemId);
+       return newItemId; // An event must be emitted here
    }
+
+
+/**
+  calldata can only be used for function parameters 
+  immutable
+
+ /** 
+
+  This functionality can be implemented  
+
+  //  function buyNFT (NFTAttributes calldata _NFT) public payable returns (uint256)
+  //  {
+  //     require(msg.value >= _NFT.price , "Insufficient funds to buy NFT");
+  //     // Refactor using error 
+
+  //  }
+
+ */
+   
+   function setDefaultRoyalty(uint96 feeNumerator) external onlyOwner
+   {
+    _setDefaultRoyalty(royaltyReceiver, feeNumerator);
+   }
+
+  function setTokenRoyalty(
+        uint256 tokenId,
+        uint96 feeNumerator
+    ) external onlyOwner {
+        _setTokenRoyalty(tokenId, royaltyReceiver, feeNumerator);
+    }
+
+     function deleteDefaultRoyalty() external onlyOwner {
+        _deleteDefaultRoyalty();
+    }
+
+
+
+
 
    // To implement royalties, we need to override the methods in ERC721URIStorage
    // The methods include transferFrom(from, to tokenID), safeTansferFrom(from, to tokenID), safeTansferFrom(from, to, tokenID, data)
@@ -76,6 +164,13 @@ contract NFT is ERC721URIStorage {
      safeTransferFrom(from, to, tokenId, '');
    }
 
+/**
+
+@dev
+
+ */
+
+
   function safeTransferFrom(
     address from,
     address to,
@@ -94,6 +189,8 @@ contract NFT is ERC721URIStorage {
 
    function _payTxFee(address from) internal {
     IERC20 token = IERC20(txFeeToken);
-    token.transferFrom(from, artist, txFeeAmount);
+    token.transferFrom(from, royaltyReceiver, txFeeAmount);
   }
 }
+
+
